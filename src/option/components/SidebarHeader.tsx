4033a1ca6d70fa36d5ce49d0ccaf1380269b01a9
6-keem <6ukeem@gmail.com>
2026-03-17 18:15:53 +0900

    chore: 옵션 페이지 삭제

--- src/option/components/SidebarHeader.tsx
+++
@@ -1,9 +0,0 @@
-import icon from '@/assets/icon.png';
-
-export function SidebarHeader() {
-  return (
-    <div className="sticky top-0 z-10 flex items-center justify-center bg-white zinc-900">
-      <img src={icon} width={80} height={80} className="p-4 rounded-full" />
-    </div>
-  );
-}
